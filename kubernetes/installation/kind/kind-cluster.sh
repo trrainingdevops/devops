57719a8c090efe8543e196027f92481b7b1ffb50
trrainingdevops <trrainingdevops@gmail.com>
2023-10-15 11:16:16 +0530

    docs: updating kind script

--- kubernetes/installation/kind/kind-cluster.sh
+++ kubernetes/installation/kind/kind-cluster.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # For AMD64 / x86_64
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-arm64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 
@@ -20,4 +22,4 @@ EOF
 sudo kind create cluster --name k8s-playground --config kind-config.yaml
 
 ## to delete kind cluster use below command ##
-## sudo kind delete cluster --name k8s-playground ##
+## kind delete cluster --name k8s-playground ##
